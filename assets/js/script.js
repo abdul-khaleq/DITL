b3eb51dd7d15dc72c9266c2ab1cc817d4ae56142
Abdul Khaleq <abdulkhaleq6008@gmail.com>
2022-11-18 14:23:01 +0600

    Added project to repositor

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+// SDK code 
+window.fbAsyncInit = function () {
+    FB.init({
+      xfbml: true,
+      version: 'v15.0'
+    });
+  };
+
+  (function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+
+//   Script for Messenger 
+var chatbox = document.getElementById('fb-customer-chat');
+chatbox.setAttribute("page_id", "106955882222221");
+chatbox.setAttribute("attribution", "biz_inbox");
+
+// Back to top button
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// AOS
